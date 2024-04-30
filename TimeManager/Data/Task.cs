fa8be30325931f47d919a2848708cf36bfc3067f
Hyunsoo Shin (Coldot) <41678750+Coldot@users.noreply.github.com>
2024-04-30 16:52:57 +0900

    feat: define `Task`

---
+++ TimeManager/Data/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager.Data
+{
+    class Task
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public long Duration { get; set; }
+    }
+}
