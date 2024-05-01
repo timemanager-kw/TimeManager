97ee7fa96e311f9cc6a85b0ab698b2aa589307de
Hyunsoo Shin (Coldot) <41678750+Coldot@users.noreply.github.com>
2024-05-01 17:57:48 +0900

    define `WeeklyDateTimeBlock`

---
+++ TimeManager/Data/Model/WeeklyDateTimeBlock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager.Data.Model
+{
+    class WeeklyDateTimeBlock
+    {
+        public DayOfWeek DayOfWeek { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
