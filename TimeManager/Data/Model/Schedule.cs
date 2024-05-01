31eb0dbda1c0f49c202b9f39cc8c0b90a0cd082b
Hyunsoo Shin (Coldot) <41678750+Coldot@users.noreply.github.com>
2024-05-01 15:53:07 +0900

    feat: define `Schedule`

---
+++ TimeManager/Data/Model/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager.Data
+{
+    abstract class Schedule
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
