77b94b9c2ac3ac9d78b1bd9a6e3815cd721a846c
Hyunsoo Shin (Coldot) <41678750+Coldot@users.noreply.github.com>
2024-05-02 03:07:05 +0900

    feat: define `TimeTable`

---
+++ TimeManager/Data/Model/TimeTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager.Data.Model
+{
+    class TimeTable
+    {
+        private DateTimeBlock[] AvailableTimes { get; set; }
+        private AssignedSchedule[] AssignedSchedules { get; set; }
+        private AssignedTask[] AssignedTasks { get; set; }
+    }
+}
