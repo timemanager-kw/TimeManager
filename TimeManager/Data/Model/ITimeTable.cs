cefa78408fa16641d923557bb783117698e4a9b1
Hyunsoo Shin (Coldot) <41678750+Coldot@users.noreply.github.com>
2024-05-03 02:38:38 +0900

    feat: define `ITimeTable`

---
+++ TimeManager/Data/Model/ITimeTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager.Data.Model
+{
+    interface ITimeTable
+    {
+        void SetWorkTimes(List<DateTimeBlock> workTimes);
+        List<DateTimeBlock> GetWorkTimes();
+
+        List<DateTimeBlock> GetAvailableTimes(DateTime week);
+
+        void AssignSchedule(AssignedSchedule assignedSchedule);
+        void UnassignSchedule(AssignedSchedule assignedSchedule);
+        List<AssignedSchedule> GetAssignedSchedules();
+
+        void AssignTask(AssignedTask assignedTask);
+        void UnassignTask(AssignedTask assignedTask);
+        List<AssignedTask> GetAssignedTasks();
+    }
+}
