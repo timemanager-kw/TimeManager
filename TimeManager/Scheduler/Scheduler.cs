e409ac848e93fff4682be776aa73e2158e7af723
Hyunsoo Shin (Coldot) <41678750+Coldot@users.noreply.github.com>
2024-05-03 02:23:39 +0900

    feat: define `Scheduler`

---
+++ TimeManager/Scheduler/Scheduler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager.Scheduler
+{
+    abstract class Scheduler
+    {
+        public void Run()
+        {
+            AssignSchdules();
+            Schedule();
+        }
+
+        abstract public void AssignSchdules();
+        abstract public void Schedule();
+    }
+}
