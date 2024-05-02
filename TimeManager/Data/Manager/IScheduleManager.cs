39d722631f7452864ef473440572030cb775efaa
Hyunsoo Shin (Coldot) <41678750+Coldot@users.noreply.github.com>
2024-05-03 02:26:59 +0900

    feat: define `IScheduleManager`

--- TimeManager/Data/Manager/IScheduleManager.cs
+++ TimeManager/Data/Manager/IScheduleManager.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeManager.Data.Model;
 
 namespace TimeManager.Data.Manager
 {
-    internal interface IScheduleManager
+    interface IScheduleManager
     {
+        void Add(Schedule schedule);
+        void Update(Schedule schedule);
+        void Delete(Schedule schedule);
+        IEnumerable<Schedule> GetAll();
     }
 }
