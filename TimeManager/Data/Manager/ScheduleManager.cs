1a715e70d60140d921fc2afa82ecc0194ccc1c1b
Hyunsoo Shin (Coldot) <41678750+Coldot@users.noreply.github.com>
2024-05-03 15:37:31 +0900

    feat: create `ScheduleManager`

---
+++ TimeManager/Data/Manager/ScheduleManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeManager.Data.Model;
+
+namespace TimeManager.Data.Manager
+{
+    class ScheduleManager : IScheduleManager
+    {
+        public void Add(Schedule schedule)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Schedule schedule)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Schedule> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Schedule schedule)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
