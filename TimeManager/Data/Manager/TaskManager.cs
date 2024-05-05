d130db3dd9f5c54e51a9e0e9d0efc4261a11c6fe
Hyunsoo Shin (Coldot) <41678750+Coldot@users.noreply.github.com>
2024-05-05 18:42:30 +0900

    feat: create `TaskManager`

---
+++ TimeManager/Data/Manager/TaskManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager.Data.Manager
+{
+    class TaskManager : ITaskManager
+    {
+        public void Add(Model.Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Model.Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.Task> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Model.Task task)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
