f1d95aa697f92ecdc142c2981df5c627525974a1
Hyunsoo Shin (Coldot) <41678750+Coldot@users.noreply.github.com>
2024-05-05 19:15:50 +0900

    feat: define members and inject dependencies

--- TimeManager/Data/Manager/TaskManager.cs
+++ TimeManager/Data/Manager/TaskManager.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TimeManager.Data.Model;
+using TimeManager.Data.Repository;
 
 namespace TimeManager.Data.Manager
 {
     class TaskManager : ITaskManager
     {
+        private readonly ITaskRepository _taskRepository;
+        private readonly List<Task> _tasks;
+
+        public TaskManager(ITaskRepository taskRepository, List<Task> tasks)
+        {
+            _taskRepository = taskRepository;
+            _tasks = tasks;
+        }
+
         public void Add(Task task)
         {
             throw new NotImplementedException();
