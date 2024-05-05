9d744e5896fbf54d0684d1dce4896fb51dd95f26
Hyunsoo Shin (Coldot) <41678750+Coldot@users.noreply.github.com>
2024-05-06 03:28:48 +0900

    feat: create `TimeTableManager`

---
+++ TimeManager/Data/Manager/TimeTableManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeManager.Data.Model;
+
+namespace TimeManager.Data.Manager
+{
+    class TimeTableManager : ITimeTableManager
+    {
+        public void Add(TimeTable timeTable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TimeTable timeTable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TimeTable Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TimeTable timeTable)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
