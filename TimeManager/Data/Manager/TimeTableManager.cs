36b5c724cd125471a56dab8e6594cc6d719cf851
Hyunsoo Shin (Coldot) <41678750+Coldot@users.noreply.github.com>
2024-05-06 03:57:16 +0900

    feat: define members and inject dependency

--- TimeManager/Data/Manager/TimeTableManager.cs
+++ TimeManager/Data/Manager/TimeTableManager.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TimeManager.Data.Model;
+using TimeManager.Data.Repository;
 
 namespace TimeManager.Data.Manager
 {
     class TimeTableManager : ITimeTableManager
     {
+        private readonly ITimeTableRepository _timeTableRepository;
+        private readonly TimeTable _timeTable;
+
+        public TimeTableManager(ITimeTableRepository timeTableRepository)
+        {
+            _timeTableRepository = timeTableRepository;
+            _timeTable = _timeTableRepository.Load();
+        }
+
         public void Add(TimeTable timeTable)
         {
             throw new NotImplementedException();
