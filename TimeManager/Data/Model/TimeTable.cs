a135915ef5927ad4370d790ed0bc5b1d942db0ab
Coldot <41678750+Coldot@users.noreply.github.com>
2024-05-08 17:14:09 +0900

    feat: define properties and constructors in `TimeTable`

--- TimeManager/Data/Model/TimeTable.cs
+++ TimeManager/Data/Model/TimeTable.cs
@@ -8,8 +8,28 @@ namespace TimeManager.Data.Model
 {
     class TimeTable
     {
-        private List<DateTimeBlock> WorkTimes { get; set; }
-        private List<AssignedSchedule> AssignedSchedules { get; set; }
-        private List<AssignedTask> AssignedTasks { get; set; }
+        /* private properties */
+        private List<DateTimeBlock> _workTimes { get; set; }
+        private List<AssignedSchedule> _assignedSchedules { get; set; }
+        private List<AssignedTask> _assignedTasks { get; set; }
+
+        /* public readonly properties */
+        public List<DateTimeBlock> WorkTimes { get { return _workTimes; } }
+        public List<AssignedSchedule> AssignedSchedules { get { return _assignedSchedules; } }
+        public List<AssignedTask> AssignedTasks { get { return _assignedTasks; } }
+
+        /* constructor */
+        public TimeTable()
+        {
+            _workTimes = new List<DateTimeBlock>();
+            _assignedSchedules = new List<AssignedSchedule>();
+            _assignedTasks = new List<AssignedTask>();
+        }
+        public TimeTable(List<DateTimeBlock> workTimes, List<AssignedSchedule> assignedSchedules, List<AssignedTask> assignedTasks)
+        {
+            _workTimes = workTimes;
+            _assignedSchedules = assignedSchedules;
+            _assignedTasks = assignedTasks;
+        }
     }
 }
