2c066beee3b145872c05cdc1037f74414e6d30dd
Coldot <41678750+Coldot@users.noreply.github.com>
2024-05-08 13:48:52 +0900

    feat: define `DateTimeExtension`

---
+++ TimeManager/Extensions/DateTimeExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager.Extensions
+{
+    public static class DateTimeExtension
+    {
+        public static DateTime StartOfWeek(this DateTime dateTime, DayOfWeek startDayOfWeek = DayOfWeek.Monday)
+        {
+            int diff = (7 + (dateTime.DayOfWeek - startDayOfWeek)) % 7;
+            return dateTime.AddDays(-1 * diff).Date;
+        }
+
+        public static DateTime EndOfWeek(this DateTime dateTime, DayOfWeek startDayOfWeek = DayOfWeek.Monday)
+        {
+            return dateTime.StartOfWeek(startDayOfWeek).AddDays(6);
+        }
+
+        public static DateTime StartOfMonth(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, 1);
+        }
+
+        public static DateTime EndOfMonth(this DateTime dateTime)
+        {
+            return dateTime.StartOfMonth().AddMonths(1).AddDays(-1);
+        }
+    }
+}
