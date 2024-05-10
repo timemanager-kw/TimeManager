126afd25523cd141e1bc9d5bfe37ae1bdfafcd65
Coldot <41678750+Coldot@users.noreply.github.com>
2024-05-11 03:24:56 +0900

    feat: declare `GetDailyAssignedSchedules()`

--- TimeManager/Data/Model/TimeTable.Operations.AssignedSchedules.cs
+++ TimeManager/Data/Model/TimeTable.Operations.AssignedSchedules.cs
@@ -52,5 +52,10 @@ namespace TimeManager.Data.Model
         {
             throw new NotImplementedException();
         }
+
+        private List<DateTimeBlock> GetDailyAssignedSchedules(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
