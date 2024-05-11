796887d77c67c791a2c41d2b9f207758152aa11b
승호 염 <99470504+YeomSH111@users.noreply.github.com>
2024-05-11 23:05:48 +0900

    feat ReplicaOfTask 클래스 생성 및 duplicate_task 메서드 작성

--- TimeManager/Scheduler/ThunderSchedulerStrategy.cs
+++ TimeManager/Scheduler/ThunderSchedulerStrategy.cs
@@ -26,7 +26,7 @@ namespace TimeManager.Scheduler
         //         (Vector<Task>) ← ∵ TaskBlock에는 Task정보만 들어가면 될듯해서
         // 
 
-
+        // Day에 여러 정보를 담아 하나로 채우는게 목적인 클래스
         private class TempBlock
         {
             public TempBlock(Data.Model.Task task) { this.task = task; }
@@ -47,27 +47,52 @@ namespace TimeManager.Scheduler
             // 가용시간이 몇시간인지(1당 30분)
             public int availableTime {  get; set; }
             // 
-            public List<TempBlock> tempBlocks;
+            public List<TempBlock> tempBlocks { get; set; }
             // 시간이 얼마나 채워졌는지
-            public int time_allocated;
+            public int time_allocated { get; set; }
             // 시간 위치에 관계없이 앞에서부터 task 쌓기
             public Data.Model.Task[] task_arr = new Data.Model.Task[48];
+        }
 
+        private class ReplicaOfTask
+        {
+            public long id;
+            public DateTime? focusDate;
+            public DateTime? endDate;
+            public int Duration; // 몇시간만에 끝낼 작업인지
+        }
+
+
+        private List<ReplicaOfTask> duplicate_task(List<Data.Model.Task> tasks)
+        {
+            List<ReplicaOfTask> repl_tasks = new List<ReplicaOfTask>();
+
+            // List<Task>에 있는 Task의 replica를 만들어 List<ReplicaOfTask>를 생성.
+            foreach(Data.Model.Task task in tasks)
+            {
+                ReplicaOfTask task_dup = new ReplicaOfTask();
+                task_dup.id = task.Id;
+                task_dup.focusDate = task.EndDate?.AddDays(-(double)task.FocusDays);
+                task_dup.endDate = task.EndDate;
+                task_dup.Duration = task.Duration.Value.Minutes / 30;
+
+                repl_tasks.Add(task_dup);
+            }
+            return repl_tasks;
         }
 
 
         public void Schedule(TimeTable timeTable, List<Data.Model.Task> tasks)
         {
-
             // Let We have AvailableTime of each day.
             // 날짜별로 가용시간 총량을 받아옴.
-
             
             List<DateTimeBlock> dateTimeBlocks =  timeTable.GetAvailableTimesInThisWeekAsOfNow();
 
+
+
             List<Day> days = new List<Day>();
             bool exist = false;
-
 
             // days에 day들을 추가하고 availableTime을 결정하는 반복문
             foreach (DateTimeBlock dateTimeBlock in dateTimeBlocks)
@@ -93,7 +118,22 @@ namespace TimeManager.Scheduler
                 }
             }
 
-            // post : days에 각 day별로 date와 
+            // post : days에 각 day별로 date와 AvailableTime이 들어감
+
+            // W.T.D : 마감 있는 Task를 배치하기 
+            foreach(Data.Model.Task task in tasks)
+            {
+
+            }
+
+
+
+
+
+
+
+
+
 
 
 
