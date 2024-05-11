f79a70234d856e6196d1666d0133402f13dfa2be
Coldot <41678750+Coldot@users.noreply.github.com>
2024-05-12 01:00:06 +0900

    test: create `TestWeeklyTimeTableControl`

---
+++ TimeManager/Forms/Tests/TestWeeklyTimeTableControl.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace TimeManager.Forms.Tests
+{
+    partial class TestWeeklyTimeTableControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.weeklyTimeTableControl1 = new TimeManager.Controls.WeeklyTimeTableControl();
+            this.SuspendLayout();
+            // 
+            // weeklyTimeTableControl1
+            // 
+            this.weeklyTimeTableControl1.Location = new System.Drawing.Point(72, 38);
+            this.weeklyTimeTableControl1.Name = "weeklyTimeTableControl1";
+            this.weeklyTimeTableControl1.Size = new System.Drawing.Size(767, 321);
+            this.weeklyTimeTableControl1.TabIndex = 0;
+            this.weeklyTimeTableControl1.timeTable = null;
+            // 
+            // TestWeeklyTimeTableControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 449);
+            this.Controls.Add(this.weeklyTimeTableControl1);
+            this.Name = "TestWeeklyTimeTableControl";
+            this.Text = "TestWeeklyTimeTableControl";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controls.WeeklyTimeTableControl weeklyTimeTableControl1;
+    }
+}
