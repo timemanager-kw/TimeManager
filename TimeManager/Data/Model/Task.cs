43a3c07a83f2039ffe3256eb1a474cef0aee59a3
Coldot <41678750+Coldot@users.noreply.github.com>
2024-05-11 21:52:36 +0900

    feat: `ETaskType` 추가 및 주석 추가

--- TimeManager/Data/Model/Task.cs
+++ TimeManager/Data/Model/Task.cs
@@ -6,16 +6,47 @@ using System.Threading.Tasks;
 
 namespace TimeManager.Data.Model
 {
+    enum ETaskType
+    {
+        /// <summary>
+        /// 마감기한이 있는 짧은 기간의 Task입니다.
+        /// </summary>
+        ShortTerm,
+
+        /// <summary>
+        /// 매주 정기적으로 반복되길 희망하는 장기적인 Task입니다.
+        /// </summary>
+        LongTerm
+    }
+
     class Task
     {
         public long Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public ETaskType Type { get; set; }
+
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
+
+        /// <summary>
+        /// Type이 ShortTerm일 때에만 존재합니다.
+        /// </summary>
         public TimeSpan? Duration { get; set; }
+
+        /// <summary>
+        /// Type이 ShortTerm일 때에만 존재합니다.
+        /// </summary>
         public int? FocusDays { get; set; }
+
+        /// <summary>
+        /// Type이 LongTerm일 때에만 존재합니다.
+        /// </summary>
         public List<WeeklyDateTimeBlock> WeeklyTimesWanted { get; set; }
+        /// <summary>
+        /// Type이 LongTerm일 때에만 존재합니다.
+        /// </summary>
         public int? NDaysOfWeekWanted { get; set; }
 
     }
