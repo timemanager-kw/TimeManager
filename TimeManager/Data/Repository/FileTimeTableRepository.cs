5efb487358c676341b2402821b284bb2aec521ca
woongjee <jeewoong7753@naver.com>
2024-05-13 20:50:19 +0900

    feat: Update() 메서드 구현

--- TimeManager/Data/Repository/FileTimeTableRepository.cs
+++ TimeManager/Data/Repository/FileTimeTableRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,20 +23,33 @@ namespace TimeManager.Data.Repository
                 }
             }
         }
-        //TimeTable의 WorkTimes, AssignedSchedules, AssignedTasks 속성을 저장/로드
+
         public void Update(TimeTable timeTable)
         {
-            List<string> lines = File.ReadAllLines(filePath).ToList();
-
+            using(StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach(var workTime in timeTable.WorkTimes)
+                {
+                    writer.WriteLine($"{workTime.StartDate}, {workTime.EndDate}, {workTime.Duration}");
+                }
+                foreach(var schedule in timeTable.AssignedSchedules)
+                {
+                    string assignedBlocks = string.Join(",", schedule.AssignedBlocks.Select(block => $"{block.StartDate}, {block.EndDate}, {block.Duration}"));
+                    writer.WriteLine($"{schedule.ScheduleId}, {schedule.ScheduleName}, {assignedBlocks}");
+                }
+                foreach(var task in timeTable.AssignedTasks)
+                {
+                    string assignedBlocks = string.Join(",",task.AssignedBlocks.Select(block =>$"{block.StartDate}, {block.EndDate}, {block.Duration}"));
+                    writer.WriteLine($"{task.TaskId}, {task.TaskName}, {assignedBlocks}");
+                }
+            }
         }
         public void Clear()
         {
-
         }
         public TimeTable Load()
         {
-            TimeTable timeTable = new TimeTable();
-            return timeTable;
+            
         }
     }
 }
