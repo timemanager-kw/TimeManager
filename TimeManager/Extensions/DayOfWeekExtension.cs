75922c7f40b36d0b851d5fe7c98a7a66302624f9
Coldot <41678750+Coldot@users.noreply.github.com>
2024-05-14 13:57:13 +0900

    feature: define `GetDayOfWeekIndexStartedFrom()`

---
+++ TimeManager/Extensions/DayOfWeekExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager.Extensions
+{
+    public static class DayOfWeekExtension
+    {
+        public static int GetDayOfWeekIndexStartedFrom(this DayOfWeek dayOfWeek, DayOfWeek start = DayOfWeek.Monday)
+        {
+            return (7 + (dayOfWeek - start)) % 7;
+        }
+    }
+}
