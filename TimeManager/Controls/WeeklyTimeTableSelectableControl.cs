04263b03f120abd8875ccafdafc864acd27da143
Coldot <41678750+Coldot@users.noreply.github.com>
2024-05-14 14:15:35 +0900

    feat: `WeeklyTimeTableControl` 복제하여 `WeeklyTimeTableSelectableControl` 생성

---
+++ TimeManager/Controls/WeeklyTimeTableSelectableControl.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TimeManager.Data.Model;
+using TimeManager.Extensions;
+
+namespace TimeManager.Controls
+{
+    public partial class WeeklyTimeTableSelectableControl : UserControl
+    {
+        public WeeklyTimeTableSelectableControl()
+        {
+            InitializeComponent();
+        }
+
+        private void WeeklyTimeTableControl_Load(object sender, EventArgs e)
+        {
+            InitializeRows();
+            dataGridView.ClearSelection();
+        }
+
+        private void InitializeRows()
+        {
+            for (int i = 0; i < 48; i++)
+            {
+                DateTime dateTime = new DateTime(2000, 1, 1, 0, 0, 0);
+                dateTime = dateTime.AddMinutes(i * 30);
+
+                dataGridView.Rows.Add();
+                dataGridView.Rows[i].HeaderCell.Value = dateTime.ToString("HH:mm");
+            }
+        }
+
+        public void CleanCells()
+        {
+            for (int i = 0; i < 48; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    dataGridView.Rows[i].Cells[j].Value = null;
+                    dataGridView.Rows[i].Cells[j].Style.BackColor = Color.LightGray;
+                }
+            }
+        }
+
+        public void DrawCells(TimeTable timeTable, Week week)
+        {
+            CleanCells();
+            DrawWorkTimes(timeTable, week);
+            DrawSchedules(timeTable, week);
+            DrawTasks(timeTable, week);
+        }
+
+        private void DrawWorkTimes(TimeTable timeTable, Week week)
+        {
+            foreach (WeeklyDateTimeBlock block in timeTable.GetWeeklyWorkTimes(week))
+            {
+                int startRow = block.StartTime.Hour * 2 + block.StartTime.Minute / 30;
+                int endRow = block.EndTime.Hour * 2 + block.EndTime.Minute / 30;
+
+                for (int i = startRow; i < endRow; i++)
+                {
+                    dataGridView.Rows[i].Cells[block.DayOfWeek.GetDayOfWeekIndex()].Style.BackColor = Color.White;
+                }
+            }
+        }
+        
+        private void DrawSchedules(TimeTable timeTable, Week week)
+        {
+            List<AssignedSchedule> schedules = timeTable.GetWeeklyAssignedSchedules(week);
+
+            foreach (AssignedSchedule schedule in schedules)
+            {
+                foreach (DateTimeBlock block in schedule.AssignedBlocks)
+                {
+                    if (!week.IsInWeek(block.StartDate)) continue;
+
+                    int startRow = block.StartDate.Hour * 2 + block.StartDate.Minute / 30;
+                    int endRow = block.EndDate.Hour * 2 + block.EndDate.Minute / 30;
+
+                    for (int i = startRow; i < endRow; i++)
+                    {
+                        dataGridView.Rows[i].Cells[block.StartDate.GetDayOfWeekIndex()].Value = "Schedule " + schedule.ScheduleId;
+                        dataGridView.Rows[i].Cells[block.StartDate.GetDayOfWeekIndex()].Style.BackColor = Color.LightBlue;
+                    }
+                }
+            }
+        }
+
+        private void DrawTasks(TimeTable timeTable, Week week)
+        {
+            List<AssignedTask> tasks = timeTable.GetWeeklyAssignedTasks(week);
+
+            foreach (AssignedTask task in tasks)
+            {
+                foreach (DateTimeBlock block in task.AssignedBlocks)
+                {
+                    if (!week.IsInWeek(block.StartDate)) continue;
+
+                    int startRow = block.StartDate.Hour * 2 + block.StartDate.Minute / 30;
+                    int endRow = block.EndDate.Hour * 2 + block.EndDate.Minute / 30;
+
+                    for (int i = startRow; i < endRow; i++)
+                    {
+                        dataGridView.Rows[i].Cells[block.StartDate.GetDayOfWeekIndex()].Value = "Task " + task.TaskId;
+                        dataGridView.Rows[i].Cells[block.StartDate.GetDayOfWeekIndex()].Style.BackColor = Color.LightGreen;
+                    }
+                }
+            }
+        }
+
+        private void dataGridView_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void dataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            // restrict selection
+            dataGridView.ClearSelection();
+        }
+
+        private void dataGridView_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            e.AdvancedBorderStyle.Bottom = DataGridViewAdvancedCellBorderStyle.None;
+
+            if (e.RowIndex < 1 || e.ColumnIndex < 0)
+            {
+                e.AdvancedBorderStyle.Top = dataGridView.AdvancedCellBorderStyle.Top;
+                return;
+            }
+
+            if (IsSameCellValue(e.ColumnIndex, e.RowIndex))
+                e.AdvancedBorderStyle.Top = DataGridViewAdvancedCellBorderStyle.None;
+            else
+                e.AdvancedBorderStyle.Top = dataGridView.AdvancedCellBorderStyle.Top;
+        }
+
+        private void dataGridView_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.RowIndex < 1) return;
+
+            if (IsSameCellValue(e.ColumnIndex, e.RowIndex))
+            {
+                e.Value = "";
+                e.FormattingApplied = true;
+            }
+        }
+
+        private bool IsSameCellValue(int column, int row)
+        {
+            DataGridViewCell cell1 = dataGridView[column, row];
+            DataGridViewCell cell2 = dataGridView[column, row - 1];
+
+            if (cell1.Value == null || cell2.Value == null)
+            {
+                return false;
+            }
+
+            return cell1.Value.Equals(cell2.Value);
+        }
+    }
+}
