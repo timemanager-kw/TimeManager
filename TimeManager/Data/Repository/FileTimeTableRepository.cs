f59b04141ab1f66e57badf8c8f476aed0fc41b7e
woongjee <jeewoong7753@naver.com>
2024-05-14 14:57:57 +0900

    feat: DateTimeBlock 생성자 추가

--- TimeManager/Data/Repository/FileTimeTableRepository.cs
+++ TimeManager/Data/Repository/FileTimeTableRepository.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using TimeManager.Data.Model;
 
 namespace TimeManager.Data.Repository
@@ -30,17 +32,17 @@ namespace TimeManager.Data.Repository
             {
                 foreach(var workTime in timeTable.WorkTimes)
                 {
-                    writer.WriteLine($"{workTime.StartDate}, {workTime.EndDate}, {workTime.Duration}");
+                    writer.WriteLine($"{"WorkTimes:"}, {workTime.StartDate}, {workTime.EndDate}, {workTime.Duration}");
                 }
                 foreach(var schedule in timeTable.AssignedSchedules)
                 {
                     string assignedBlocks = string.Join(",", schedule.AssignedBlocks.Select(block => $"{block.StartDate}, {block.EndDate}, {block.Duration}"));
-                    writer.WriteLine($"{schedule.ScheduleId}, {schedule.ScheduleName}, {assignedBlocks}");
+                    writer.WriteLine($"{"AssignedSchedules:"}, {schedule.ScheduleId}, {schedule.ScheduleName}, {assignedBlocks}");
                 }
                 foreach(var task in timeTable.AssignedTasks)
                 {
                     string assignedBlocks = string.Join(",",task.AssignedBlocks.Select(block =>$"{block.StartDate}, {block.EndDate}, {block.Duration}"));
-                    writer.WriteLine($"{task.TaskId}, {task.TaskName}, {assignedBlocks}");
+                    writer.WriteLine($"{"AssignedTasks"}, {task.TaskId}, {task.TaskName}, {assignedBlocks}");
                 }
             }
         }
@@ -50,7 +52,7 @@ namespace TimeManager.Data.Repository
         }
         public TimeTable Load()
         {
-            
+        
         }
     }
 }
