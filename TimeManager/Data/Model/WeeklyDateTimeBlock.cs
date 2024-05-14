4333af881a8f27e9e5942d40514ad5d85e466f5d
Coldot <41678750+Coldot@users.noreply.github.com>
2024-05-14 15:32:28 +0900

    feat: define constructor `WeeklyDateTimeBlock()`

--- TimeManager/Data/Model/WeeklyDateTimeBlock.cs
+++ TimeManager/Data/Model/WeeklyDateTimeBlock.cs
@@ -11,5 +11,12 @@ namespace TimeManager.Data.Model
         public DayOfWeek DayOfWeek { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
+
+        public WeeklyDateTimeBlock(DayOfWeek dayOfWeek, DateTime startTime, DateTime endTime)
+        {
+            DayOfWeek = dayOfWeek;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
     }
 }
