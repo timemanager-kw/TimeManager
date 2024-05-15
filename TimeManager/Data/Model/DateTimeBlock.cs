b0266db4daeb444916d112e41955be5320a30955
woongjee <jeewoong7753@naver.com>
2024-05-15 15:39:53 +0900

    feat: difference메서드 구현

--- TimeManager/Data/Model/DateTimeBlock.cs
+++ TimeManager/Data/Model/DateTimeBlock.cs
@@ -28,8 +28,45 @@ namespace TimeManager.Data.Model
 
         public static IEnumerable<DateTimeBlock> Difference(IEnumerable<DateTimeBlock> minuend, IEnumerable<DateTimeBlock> subtrahend)
         {
-
-            throw new NotImplementedException();
+            List<DateTimeBlock> difference = new List<DateTimeBlock>();
+            foreach(var minuendBlock in minuend)
+            {
+                bool overlap = false;
+                foreach(var subtrahendBlock in subtrahend)
+                {
+                    if(!(minuendBlock.EndDate <= subtrahendBlock.StartDate)|| minuendBlock.StartDate >= subtrahendBlock.EndDate){
+                        overlap = true;
+                        if ((minuendBlock.StartDate <= subtrahendBlock.StartDate) && minuendBlock.EndDate <= subtrahendBlock.EndDate) {
+                            DateTimeBlock canTime = new DateTimeBlock();
+                            canTime.StartDate = minuendBlock.StartDate;
+                            canTime.EndDate = subtrahendBlock.StartDate;
+                            difference.Add(canTime);
+                        }
+                        else if((minuendBlock.StartDate>=subtrahendBlock.StartDate) && (minuendBlock.EndDate >= subtrahendBlock.EndDate))
+                        {
+                            DateTimeBlock canTIme = new DateTimeBlock();
+                            canTIme.StartDate = minuendBlock.StartDate;
+                            canTIme.EndDate = subtrahendBlock.EndDate;
+                            difference.Add(canTIme);
+                        }
+                        else if ((minuendBlock.StartDate >= subtrahendBlock.StartDate) && (subtrahendBlock.EndDate <= minuendBlock.EndDate))
+                        {
+                            DateTimeBlock canTime = new DateTimeBlock();
+                            canTime.StartDate = minuendBlock.StartDate;
+                            canTime.EndDate = subtrahendBlock.StartDate;
+                            difference.Add(canTime);
+                            canTime.StartDate = subtrahendBlock.StartDate;
+                            canTime.EndDate = minuendBlock.EndDate;
+                            difference.Add(canTime);
+                        }
+                    }
+                }
+                if (!overlap)
+                {
+                    difference.Add(minuendBlock);
+                }
+            }
+            return difference;
         }
     }
 }
