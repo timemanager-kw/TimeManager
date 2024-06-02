ea1ae1dbf7a567256821f286d042e938e8f3445a
woongjee <jeewoong7753@naver.com>
2024-06-02 22:41:13 +0900

    feat: longTermProperties 구조체 생성자 추가

--- TimeManager/Data/Model/longTermProperties.cs
+++ TimeManager/Data/Model/longTermProperties.cs
@@ -8,6 +8,10 @@ namespace TimeManager.Data.Model
 {
     public struct longTermProperties
     {
+        public longTermProperties(DayOfWeek d, TimeSpan t){ 
+            dayOfWeek = d;
+            time = t;
+        }
         /// <summary>
         /// Type이 LongTerm일 때에만 존재합니다.
         /// </summary>
