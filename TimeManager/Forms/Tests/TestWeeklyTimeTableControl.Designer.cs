17fd351e5818032d094d6930e80cbb0d8464aea2
Coldot <41678750+Coldot@users.noreply.github.com>
2024-06-14 16:04:06 +0900

    test: `ItemSelected` 이벤트 동작 확인 용 코드 추가

--- TimeManager/Forms/Tests/TestWeeklyTimeTableControl.Designer.cs
+++ TimeManager/Forms/Tests/TestWeeklyTimeTableControl.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.weeklyTimeTableControl1 = new TimeManager.Controls.WeeklyTimeTableControl();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // weeklyTimeTableControl1
@@ -37,22 +38,34 @@
             this.weeklyTimeTableControl1.Name = "weeklyTimeTableControl1";
             this.weeklyTimeTableControl1.Size = new System.Drawing.Size(825, 321);
             this.weeklyTimeTableControl1.TabIndex = 0;
+            this.weeklyTimeTableControl1.ItemSelected += new System.EventHandler<TimeManager.Controls.WeeklyTimeTableControlItemEventArgs>(this.weeklyTimeTableControl1_ItemSelected);
             this.weeklyTimeTableControl1.Load += new System.EventHandler(this.weeklyTimeTableControl1_Load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 1;
             // 
             // TestWeeklyTimeTableControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(925, 449);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.weeklyTimeTableControl1);
             this.Name = "TestWeeklyTimeTableControl";
             this.Text = "TestWeeklyTimeTableControl";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Controls.WeeklyTimeTableControl weeklyTimeTableControl1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- TimeManager/Forms/Tests/TestWeeklyTimeTableControl.cs
+++ TimeManager/Forms/Tests/TestWeeklyTimeTableControl.cs
@@ -27,10 +27,20 @@ namespace TimeManager.Forms.Tests
             assignedSchedule1.ScheduleName = "약속";
             assignedSchedule1.AssignedBlocks.Add(new DateTimeBlock(new DateTime(2024, 5, 6, 8, 0, 0), new DateTime(2024, 5, 6, 12, 0, 0)));
 
+            AssignedSchedule assignedSchedule2 = new AssignedSchedule();
+            assignedSchedule2.ScheduleId = 2;
+            assignedSchedule2.ScheduleName = "강의";
+            assignedSchedule2.AssignedBlocks.Add(new DateTimeBlock(new DateTime(2024, 5, 8, 9, 0, 0), new DateTime(2024, 5, 8, 12, 0, 0)));
+
             AssignedTask assignedTask1 = new AssignedTask();
             assignedTask1.TaskId = 1;
             assignedTask1.TaskName = "응소실 과제";
             assignedTask1.AssignedBlocks.Add(new DateTimeBlock(new DateTime(2024, 5, 6, 13, 0, 0), new DateTime(2024, 5, 6, 17, 0, 0)));
+
+            AssignedTask assignedTask2 = new AssignedTask();
+            assignedTask2.TaskId = 2;
+            assignedTask2.TaskName = "운영체제 과제";
+            assignedTask2.AssignedBlocks.Add(new DateTimeBlock(new DateTime(2024, 5, 9, 15, 0, 0), new DateTime(2024, 5, 9, 22, 0, 0)));
 
             List<DateTimeBlock> workTimes = new List<DateTimeBlock>();
             for (int i = 0; i < 7; i++)
@@ -38,7 +48,9 @@ namespace TimeManager.Forms.Tests
                 workTimes.Add(new DateTimeBlock(new DateTime(2024, 5, 6 + i, 8, 0, 0), new DateTime(2024, 5, 6 + i, 22, 0, 0)));
             }
 
-            timeTable = new TimeTable(workTimes, new List<AssignedSchedule> { assignedSchedule1 }, new List<AssignedTask> { assignedTask1 });
+            timeTable = new TimeTable(workTimes, 
+                new List<AssignedSchedule> { assignedSchedule1, assignedSchedule2 }, 
+                new List<AssignedTask> { assignedTask1, assignedTask2 });
 
             Week week = new Week();
             week.Year = 2024;
@@ -46,6 +58,11 @@ namespace TimeManager.Forms.Tests
             week.WeekOfMonth = 1;
 
             weeklyTimeTableControl1.DrawCells(timeTable, week);
+        }
+
+        private void weeklyTimeTableControl1_ItemSelected(object sender, Controls.WeeklyTimeTableControlItemEventArgs e)
+        {
+            label1.Text = "Selected: " + e.AssignedItemType.ToString() + " - " + e.AssignedItemId.ToString();
         }
     }
 }
