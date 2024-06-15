9e865fe5de9002b247f074b9929917c8171700a2
woongjee <jeewoong7753@naver.com>
2024-06-15 18:49:42 +0900

    fix : AssignedSchedule에서 ScheduleName 삭제

--- TimeManager/Data/Model/AssignedSchedule.cs
+++ TimeManager/Data/Model/AssignedSchedule.cs
@@ -11,11 +11,9 @@ namespace TimeManager.Data.Model
         public AssignedSchedule(List<DateTimeBlock> assignedblocks, long scheduleid, string schedulename) {
             AssignedBlocks = assignedblocks;
             ScheduleId = scheduleid;
-            ScheduleName = schedulename;
         }
         public List<DateTimeBlock> AssignedBlocks { get; set; }
         public long ScheduleId { get; set; }
-        public string ScheduleName { get; set; }
 
         public AssignedSchedule()
         {
