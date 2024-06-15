58ddc26be54d60eaf76f5caeb77100b4263ff641
woongjee <jeewoong7753@naver.com>
2024-06-15 19:21:11 +0900

    fix: timeTable.AssignmentTasks에서 _assignedTasks로 변수명 수정

--- TimeManager/Data/Model/TimeTable.Operations.AssignedTasks.cs
+++ TimeManager/Data/Model/TimeTable.Operations.AssignedTasks.cs
@@ -8,26 +8,25 @@ namespace TimeManager.Data.Model
 {
     public partial class TimeTable : ITimeTable
     {
-        private TimeTable timeTable = new TimeTable();
         /* AssignedTask Operations */
         public void AssignTask(long taskId, IEnumerable<DateTimeBlock> assignedTimeBlocks)
         {
-           if(timeTable.AssignedTasks.Any(t =>t.TaskId == taskId))
+           if(_assignedTasks.Any(t =>t.TaskId == taskId))
             {
                 throw new ArgumentException("이미 있는 task입니다.");
             }
             var newTask = new AssignedTask(assignedTimeBlocks.ToList(), taskId);
-            timeTable.AssignedTasks.Add(newTask); 
+            _assignedTasks.Add(newTask); 
         }
 
         public void ReassignTask(long taskId, IEnumerable<DateTimeBlock> assignedTimeBlocks)
         {
-            var task = timeTable.AssignedTasks.FirstOrDefault(t=>t.TaskId == taskId);
+            var task = _assignedTasks.FirstOrDefault(t=>t.TaskId == taskId);
             if(task != null)
             {
                 task.AssignedBlocks.Clear();
                 var newTask = new AssignedTask(assignedTimeBlocks.ToList(), taskId);
-                timeTable.AssignedTasks.Add(newTask);
+                _assignedTasks.Add(newTask);
             }
             else
             {
@@ -37,10 +36,10 @@ namespace TimeManager.Data.Model
 
         public void UnassignTask(long taskId)
         {
-            var taskToRemove = timeTable.AssignedTasks.FirstOrDefault(t => t.TaskId == taskId);
+            var taskToRemove = _assignedTasks.FirstOrDefault(t => t.TaskId == taskId);
             if(taskToRemove != null)
             {
-                timeTable.AssignedTasks.Remove(taskToRemove);
+                _assignedTasks.Remove(taskToRemove);
             }
             else
             {
@@ -50,7 +49,7 @@ namespace TimeManager.Data.Model
 
         public List<AssignedTask> GetAssignedTasksByTaskId(long taskId)
         {
-            return timeTable.AssignedTasks.Where(t=>t.TaskId == taskId).ToList();
+            return _assignedTasks.Where(t=>t.TaskId == taskId).ToList();
         }
 
         public List<AssignedTask> GetAllAssignedTasks()
