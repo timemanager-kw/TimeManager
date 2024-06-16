85259d215010385e9b25427dabeaa040d3bef247
Coldot <41678750+Coldot@users.noreply.github.com>
2024-06-16 19:04:00 +0900

    fix: 변경된 의존성 스펙에 맞게 수정

--- TimeManager/Forms/Tests/TestWeeklyTimeTableControl.cs
+++ TimeManager/Forms/Tests/TestWeeklyTimeTableControl.cs
@@ -5,8 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using TimeManager.Data.Manager;
 using TimeManager.Data.Model;
 
 namespace TimeManager.Forms.Tests
@@ -14,6 +14,9 @@ namespace TimeManager.Forms.Tests
     public partial class TestWeeklyTimeTableControl : Form
     {
         private TimeTable timeTable;
+
+        private ITaskManager taskManager = new FakeTaskManager();
+        private IScheduleManager scheduleManager = new FakeScheduleManager();
 
         public TestWeeklyTimeTableControl()
         {
@@ -24,22 +27,18 @@ namespace TimeManager.Forms.Tests
         {
             AssignedSchedule assignedSchedule1 = new AssignedSchedule();
             assignedSchedule1.ScheduleId = 1;
-            assignedSchedule1.ScheduleName = "약속";
             assignedSchedule1.AssignedBlocks.Add(new DateTimeBlock(new DateTime(2024, 5, 6, 8, 0, 0), new DateTime(2024, 5, 6, 12, 0, 0)));
 
             AssignedSchedule assignedSchedule2 = new AssignedSchedule();
             assignedSchedule2.ScheduleId = 2;
-            assignedSchedule2.ScheduleName = "강의";
             assignedSchedule2.AssignedBlocks.Add(new DateTimeBlock(new DateTime(2024, 5, 8, 9, 0, 0), new DateTime(2024, 5, 8, 12, 0, 0)));
 
             AssignedTask assignedTask1 = new AssignedTask();
             assignedTask1.TaskId = 1;
-            assignedTask1.TaskName = "응소실 과제";
             assignedTask1.AssignedBlocks.Add(new DateTimeBlock(new DateTime(2024, 5, 6, 13, 0, 0), new DateTime(2024, 5, 6, 17, 0, 0)));
 
             AssignedTask assignedTask2 = new AssignedTask();
             assignedTask2.TaskId = 2;
-            assignedTask2.TaskName = "운영체제 과제";
             assignedTask2.AssignedBlocks.Add(new DateTimeBlock(new DateTime(2024, 5, 9, 15, 0, 0), new DateTime(2024, 5, 9, 22, 0, 0)));
 
             List<DateTimeBlock> workTimes = new List<DateTimeBlock>();
@@ -63,6 +62,88 @@ namespace TimeManager.Forms.Tests
         private void weeklyTimeTableControl1_ItemSelected(object sender, Controls.WeeklyTimeTableControlItemEventArgs e)
         {
             label1.Text = "Selected: " + e.AssignedItemType.ToString() + " - " + e.AssignedItemId.ToString();
+        }
+
+        private class FakeTaskManager : ITaskManager
+        {
+            List<Task> tasks = new List<Task>(
+                new Task[]
+                {
+                    new Task { Id = 1, Name = "Task1" },
+                    new Task { Id = 2, Name = "Task2" }
+                }
+            );
+
+            public void Add(Task task)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Delete(Task task)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerable<Task> GetAll()
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task GetById(long id)
+            {
+                return tasks.Find(t => t.Id == id);
+            }
+
+            public List<Task> GetTasks()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Update(Task task)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private class FakeScheduleManager : IScheduleManager
+        {
+            List<Schedule> schedules = new List<Schedule>(
+                new Schedule[]
+                {
+                    new Schedule { Id = 1, Name = "Schedule1" },
+                    new Schedule { Id = 2, Name = "Schedule2" }
+                }
+            );
+
+            public void Add(Schedule schedule)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Delete(Schedule schedule)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerable<Schedule> GetAll()
+            {
+                throw new NotImplementedException();
+            }
+
+            public Schedule GetById(long id)
+            {
+                return schedules.Find(s => s.Id == id);
+            }
+
+            public List<Schedule> GetSchedules()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Update(Schedule schedule)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
